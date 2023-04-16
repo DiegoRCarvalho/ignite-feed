a49fdad51c7c269cddb89fffc8e40f59ce602648
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-15 23:18:25 -0300

    feat: add avatar image

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,12 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
       />
-      <div className={styles.profile}></div>
+      <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/40546627?v=4"
+        />
+      </div>
     </aside>
   )
 }
