ab789a33158dfda3f6e694651e0f488fa82e36ca
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-16 13:31:34 -0300

    feat: add post header elements

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable jsx-a11y/alt-text */
 import styles from './Post.module.css'
 
 export function Post() {
-  return <h1>Feed</h1>
+  return (
+    <div>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img src="https://github.com/diegorcarvalho.png" />
+            <div className={styles.authorInfo}>
+              <strong>Autor</strong>
+              <span>Profissão</span>
+            </div>
+          </div>
+        </header>
+      </article>
+    </div>
+  )
 }
