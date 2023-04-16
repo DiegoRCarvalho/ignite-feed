d7c7b53df0246f5b400e6ad50f206a21a82197e3
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-15 23:05:23 -0300

    feat: add header elements (top bar)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,5 +3,11 @@ import styles from './Header.module.css'
 import igniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
-  return ''
+  return (
+    <div>
+      <header className={styles.header}>
+        <img src={igniteLogo} alt="logotipo do Ignite" />
+      </header>
+    </div>
+  )
 }
