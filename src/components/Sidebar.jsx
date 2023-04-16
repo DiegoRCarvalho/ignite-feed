2f2058ef807416fd7385515f6987e2752a8aa681
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-15 23:17:38 -0300

    feat: add div to group user data

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,6 +8,7 @@ export function Sidebar() {
         className={styles.cover}
         src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"
       />
+      <div className={styles.profile}></div>
     </aside>
   )
 }
