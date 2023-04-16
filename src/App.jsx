cc23a0c0046e05351a9f9e5b85da7e1941d8c248
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-15 23:00:08 -0300

    feat: create the division of the main elements of the HTML

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
+
+import './global.css'
+
 export function App() {
-  return <div className="App"></div>
+  return (
+    <div>
+      <Header />
+      <div>
+        <Sidebar />
+        <main>
+          <Post />
+        </main>
+      </div>
+    </div>
+  )
 }
