7fd5e4625390c09ba6d7ffc3e165c46f352ac8cb
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-16 13:32:29 -0300

    feat: add publish date element

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -13,6 +13,12 @@ export function Post() {
               <span>Profissão</span>
             </div>
           </div>
+          <time
+            title="11 de maio de 2023 às 08:13h"
+            dateTime="2023-04-16 08:13:00"
+          >
+            Publicado há 1h
+          </time>
         </header>
       </article>
     </div>
