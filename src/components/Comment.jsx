bd076cfb5fb03a782ff555c6fce82b2b5d922c2f
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-22 14:03:10 -0300

    feat: add function to count likes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,18 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react'
+
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
-import { ThumbsUp, Trash } from 'phosphor-react'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment(params) {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
   return (
     <div className={styles.comment}>
@@ -33,9 +41,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
