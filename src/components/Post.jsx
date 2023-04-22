c29c1e89dd57582e79f45b886fb7b8c6d78bf551
diegorcarvalho <diegoribeirodecarvalho@gmail.com>
2023-04-22 13:39:56 -0300

    feat: form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,8 @@ export function Post({ author, publishedAt, content }) {
    ])
  
   const [newCommentText, setNewCommentText] = useState('')
+
+  const isNewCommentEmpty = newCommentText.length === 0
  
 
   function handleCrateNewComment() {
@@ -42,7 +44,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('')
     setNewCommentText(event.target.value)
+  }
+
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   function deleteComment(commentToDelete) {
@@ -87,9 +94,16 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
         <footer>
-          <button type="submit">Publicar</button>
+          <button 
+            type="submit"
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
       <div className={styles.commentList}>
